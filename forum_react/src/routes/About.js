4d5c4391c94356a0fabddfcd02da845f704976fa
hckim96 <hyeoncheolkim96@gmail.com>
2020-06-19 14:49:36 +0900

    about

--- forum_react/src/routes/About.js
+++ forum_react/src/routes/About.js
@@ -12,6 +12,9 @@ const About = () => {
             <h1>6.18</h1>
             <br></br> - latest posts 를 this.state 에서 넘기는 데 바로 슬라이스를 하려니까 setstate가 비동기라 업데이트전에 가져와서 오류가 나는거 같음
             그래서 posts 에다가 boolean 하나 더 넘겨서 거기안에서 posts 컴포넌트 안에서 잘랏더니 됨
+            <br></br> -익명 글쓰기 추가함
+            
+            
         </div>
     )
 }
